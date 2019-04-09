99f05fc6bba567aa2f29523d9715319491e9ff67
Henny Yoffe <hennyy1149@gmail.com>
2019-04-08 20:44:49 -0400

    Add project files.

---
+++ Lesson57_Jason_April8/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Lesson57_Jason_April8
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
